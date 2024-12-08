eaec8583f839fc14445b0901075abdf37f60ab20
harshpatel2125 <harshpatel2125@gmail.com>
2024-12-08 14:14:35 +0530

    Test: Handle newlines as delimiters

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -17,4 +17,8 @@ describe("StringCalculator", () => {
 	it("should return the sum of two numbers separated by a comma", () => {
 		expect(calculator.add("1,2")).toBe(3);
 	});
+
+	it("should handle newlines as delimiters", () => {
+		expect(calculator.add("1\n2,3")).toBe(6);
+	});
 });
