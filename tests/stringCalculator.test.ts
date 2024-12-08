bbdcce860330bd6ffd56744e4b8123f276da39ca
harshpatel2125 <harshpatel2125@gmail.com>
2024-12-08 14:09:18 +0530

    Test: Handle two numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -9,7 +9,12 @@ describe("StringCalculator", () => {
 	it("should return 0 for an empty string", () => {
 		expect(calculator.add("")).toBe(0);
 	});
+
 	it("should return the number itself if the input is a single number", () => {
 		expect(calculator.add("1")).toBe(1);
+	});
+
+	it("should return the sum of two numbers separated by a comma", () => {
+		expect(calculator.add("1,2")).toBe(3);
 	});
 });
